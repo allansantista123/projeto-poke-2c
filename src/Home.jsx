234fa0b87138bf57dbfdfc4f1b56d3248009daf6
allansantista123 <rm94425@estudante.fieb.edu.br>
2025-05-15 12:55:37 +0000

    flex

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,6 +7,22 @@ const Home = () => {
         <div>
             <img src={poke} />
              <img src={eevee} />
+             <img src={poke} />
+             <img src={eevee} />
+             <img src={poke} />
+             <img src={eevee} />
+             <img src={poke} />
+             <img src={eevee} />
+             <img src={poke} />
+             <img src={eevee} />
+             <img src={poke} />
+             <img src={eevee} />
+             <img src={poke} />
+             <img src={eevee} />
+             <img src={poke} />
+             <img src={eevee} />
+             <img src={poke} />
+             <img src={eevee} />
         </div>
     )
 
